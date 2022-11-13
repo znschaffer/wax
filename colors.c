74fa1c5362dc34d4ca031f6fd8f196cb0bbcf936
Xinrui Chen <xyn.hello@gmail.com>
2022-11-13 14:31:30 -0800

    first windows

---
+++ colors.c
@@ -0,0 +1,7 @@
+#include "wax.h"
+
+void setupColors() {
+  start_color();
+
+  init_pair(1, COLOR_GREEN, COLOR_BLACK);
+}
--- ui.c
+++ ui.c
@@ -1,9 +1,11 @@
 #include "wax.h"
 #include <stdlib.h>
+#include <string.h>
 
 ITEM **song_items;
 MENU *song_menu;
-int n_songs = 0;
+WINDOW *song_win;
+char *title;
 
 void setupCurses() {
   initscr();
@@ -17,14 +19,34 @@ void setupCurses() {
 int setupUI() {
   int ch;
   setupCurses();
+  setupColors();
   song_items = (ITEM **)calloc(200, sizeof(ITEM *));
   populateSongItems();
   song_menu = new_menu((ITEM **)song_items);
-  post_menu(song_menu);
+
+  song_win = newwin(0, 0, 0, 0);
+  keypad(song_win, TRUE);
+
+  set_menu_win(song_menu, song_win);
+  set_menu_sub(song_menu, derwin(song_win, n_songs, COLS - 10, 3, 3));
+
+  set_menu_mark(song_menu, " > ");
+
+  /* Print a border around the main window and print a title */
+  box(song_win, 0, 0);
+  title = "wax";
+
+  // print_in_middle(song_win, 1, (COLS / 2) - (strlen(title) / 2),
+  // strlen(title),
+  //                 title, 1);
+  mvwhline(song_win, 2, 1, ACS_HLINE, COLS - 2);
   refresh();
 
+  post_menu(song_menu);
+  wrefresh(song_win);
+
   while ((ch = getch()) != KEY_F(1)) {
-    mvprintw(LINES-3, 0, "%d", getSongTime());
+    mvprintw(LINES - 3, 0, "%d", getSongTime());
     handleInput(ch);
   }
   return 0;
@@ -41,14 +63,18 @@ void handleInput(int ch) {
     menu_driver(song_menu, REQ_UP_ITEM);
     break;
   case 'p':
-    mvprintw(LINES - 2, 0, "loading song from %s\n",
-             song_menu->curitem->name.str);
+    move(1, 1);
+    clrtoeol();
+    printMiddle(song_win, 1,
+                (COLS / 2) -
+                    (strlen(getCurrTitle(song_menu->curitem->index)) / 2),
+                strlen(getCurrTitle(song_menu->curitem->index)),
+                getCurrTitle(song_menu->curitem->index), 1);
     refresh();
     loadSound(song_menu->curitem);
     break;
   }
+  wrefresh(song_win);
 }
 
-void cleanupUI() {
-  endwin();
-}
+void cleanupUI() { endwin(); }
