6289ecc75a86f01ff94d65cfffc7c8db13c1b26a
Xinrui Chen <xyn.hello@gmail.com>
2022-12-18 20:08:28 -0800

    adjust ticker ui

--- ticker.c
+++ ticker.c
@@ -3,7 +3,7 @@
 
 int SONG_CURRTIME = 0;
 int SONG_DUR = 0;
-int TICKER_START = 8;
+int TICKER_START = 9;
 int TICKER_END;
 int TICKER_HEIGHT;
 int TICKER_LENGTH;
@@ -20,7 +20,7 @@ int PLAYED_LENGTH() { return (int)(percentPlayed() * TICKER_LENGTH); }
 void drawTime(WINDOW *win) {}
 
 void drawTicker() {
-  TICKER_END = COLS - 17;
+  TICKER_END = COLS - 20;
   TICKER_HEIGHT = LINES - 4;
   TICKER_LENGTH = TICKER_END - TICKER_START;
 
